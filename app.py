ce5c72b98f5b24296583f7167157f86f1380d8bf
khangkt1310 <khangkieutrong@gmail.com>
2023-10-21 00:59:28 +0700

    edit media resource

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory(os.path.join(os.path.dirname(__file__), 'media'), path)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -16,8 +21,6 @@ def submit():
     except Exception as e:
         print('Error:', str(e))
         return jsonify({'success': False, 'error': str(e)})
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
