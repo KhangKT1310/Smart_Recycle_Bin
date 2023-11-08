c8d56c9b183ac08eb3f826ca9ece6c8db6d5bfc3
khangkt <khangkieutrong@gmail.com>
2023-11-08 11:21:54 +0700

    remove script

--- Start.sh
+++ Start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-# Mở terminal mới để chạy app.py và hiển thị log
-gnome-terminal --title="Run Log" -- bash -c 'python3 /home/khangkt/Desktop/Smart_Recycle_Bin/app.py; read'
-
-# Tiếp tục thực hiện các công việc khác
-sleep 1
-
-export DISPLAY=:0  # Đảm bảo hiển thị được set đúng
-google-chrome http://localhost:1234/
+# add to end of file /home/pi/.config/lxsession/LXDE-pi/autostart
+@python3 /home/pi/Desktop/Smart_Recycle_Bin/app.py
+@chromium-browser http://localhost:8080/
 
